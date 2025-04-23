dc20dd338b10e16741acb746ab3044234aba1f96
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-04-23 12:19:51 -0600

    18 - Storing Data to Postgres with SQLModel

--- src/api/video_events/models.py
+++ src/api/video_events/models.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
-class YouTubeWatchEvent(SQLModel, table=False):
+class YouTubeWatchEvent(SQLModel, table=True):
     id: int = Field(primary_key=True)
     is_ready: bool 
     video_id: str = Field(index=True)
@@ -11,6 +11,8 @@ class YouTubeWatchEvent(SQLModel, table=False):
     current_time: float
     video_state_label: str
     video_state_value: int
+    referer: Optional[str] = Field(default="", index=True)
+
 
 class YouTubePlayerState(SQLModel, table=False):
     is_ready: bool 
@@ -19,3 +21,9 @@ class YouTubePlayerState(SQLModel, table=False):
     current_time: float
     video_state_label: str
     video_state_value: int
+
+
+class YouTubeWatchEventResponseModel(SQLModel, table=False):
+    id: int = Field(primary_key=True)
+    video_id: str = Field(index=True)
+    current_time: float
--- src/api/video_events/routing.py
+++ src/api/video_events/routing.py
@@ -4,11 +4,11 @@ from sqlmodel import Session
 
 from api.db.session import get_session
 
-from .models import YouTubePlayerState
+from .models import YouTubePlayerState, YouTubeWatchEvent, YouTubeWatchEventResponseModel
 
 router = APIRouter()
 
-@router.post("/") # /api/video-events/
+@router.post("/", response_model=YouTubeWatchEventResponseModel) # /api/video-events/
 def create_video_event(
         request: Request, 
         payload: YouTubePlayerState,
@@ -16,7 +16,14 @@ def create_video_event(
     ):
     headers = request.headers
     referer = headers.get("referer")
-    print(db_session)
+    # print(db_session)
     data = payload.model_dump()
-    print(data, referer)
-    return data
+    # data["referer"] = referer
+    obj = YouTubeWatchEvent(**data)
+    obj.referer = referer
+    db_session.add(obj)
+    db_session.commit()
+    db_session.refresh(obj)
+    print(obj)
+    # print(data, referer)
+    return obj
